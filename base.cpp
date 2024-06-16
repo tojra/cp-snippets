e364017df8bb9855f099102852801eab52d134ba
tojra <154787376+tojra@users.noreply.github.com>
2024-06-16 20:48:35 +0530

    Create base.cpp

---
+++ base.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ff first
+#define ss second
+#define pb push_back
+#define rep(i,n) for(int i=0;i<n;i++)
+#define all(v) v.begin(),v.end()
+using ll= long long;
+const ll MOD=1e9+7;
+
+void solve()
+{
+    
+}
+
+int main()
+{
+    
+    int t;
+    t=1;
+    cin>>t;
+    while(t--)
+        solve();
+    
+}
